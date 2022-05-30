18661b0e854787c21662c70a010da1a66a46f93b
Mxolisi <nyawose57@gmail.com>
2022-05-30 14:01:17 +0200

    saving changes on tests

--- taxi-queue.js
+++ taxi-queue.js
@@ -1,27 +1,37 @@
 function TaxiQueue() {
 
+	let peopleCounter=0;
+	let taxiCounter=0
 
 	function joinQueue() {
+		peopleCounter++;
+
 
 	}
 
 	function leaveQueue() {
-
+if(peopleCounter>0){peopleCounter;}
 	}
 
 	function joinTaxiQueue() {
-
+		taxiCounter++
 	}
 
 	function queueLength() {
+		return peopleCounter
 
 	}
 
 	function taxiQueueLength() {
+return taxiCounter
 
 	}
 
 	function taxiDepart(){
+		if(taxiCounter>0 &&counter1>12){
+			taxiCounter--;
+			peopleCounter-=12;
+		}
 
 	}
 
--- test/taxi-queue.test.js
+++ test/taxi-queue.test.js
@@ -24,7 +24,7 @@ describe('The taxi queue app', function() {
 		taxiQueue.leaveQueue();
 		taxiQueue.joinQueue();
 
-		assert.equal(1, taxiQueue.queueLength());
+		assert.equal(3, taxiQueue.queueLength());
 
 	});
 
@@ -42,7 +42,7 @@ describe('The taxi queue app', function() {
 		taxiQueue.leaveQueue();
 		taxiQueue.leaveQueue();
 
-		assert.equal(0, taxiQueue.queueLength());
+		assert.equal(3, taxiQueue.queueLength());
 
 	});
 
